a4aeffcc85338714c8c4a1a45a4f442c27bd7c3d
zeqiang-lai <laizeqiang@outlook.com>
2023-05-10 21:07:18 +0800

    tmp: diff linear solver

--- tests/test_linear_solver_torch.py
+++ tests/test_linear_solver_torch.py
@@ -1,4 +1,6 @@
 import torch
+import torch.autograd
+
 import dprox
 
 
@@ -94,37 +96,63 @@ def test_linear_solver_torch_backward_dtheta():
 
     P = torch.randn(5, 5).requires_grad_(True)
     A = P.T @ P
+    A.retain_grad()
+    # A = A.clone().detach().requires_grad_(True)
     x = torch.randn(5)
     b = A @ x
     b = b.clone().detach().requires_grad_(True)
 
-    A = LinOp(A)
+    K = LinOp(A)
 
-    xhat = dprox.proxfn.linalg.linear_solve(A, b)
+    xhat = dprox.proxfn.linalg.linear_solve(K, b)
 
     xhat.mean().backward()
 
+    # grad1 = A.A.grad
     grad1 = P.grad
+    gradA1 = A.grad
     
     # gradient with unrolling
     torch.manual_seed(seed)
 
-    P = torch.randn(5, 5).requires_grad_(True)
+    # P = torch.randn(5, 5)
+    P = torch.randn(5, 5).requires_grad_(True) 
     A = P.T @ P
+    A.retain_grad()
+    # A = A.clone().detach().requires_grad_(True)
     x = torch.randn(5)
     b = A @ x
     b = b.clone().detach().requires_grad_(True)
 
-    A = LinOp(A)
+    K = LinOp(A)
 
-    xhat = dprox.proxfn.linalg.solve.conjugate_gradient(A, b)
+    xhat = dprox.proxfn.linalg.solve.conjugate_gradient(K, b)
 
     xhat.mean().backward()
 
+    # grad2 = A.A.grad
     grad2 = P.grad
+    gradA2 = A.grad
     
-    print(grad1)
-    print(grad2)
+    print(grad1.numpy())
+    print(grad2.numpy())
+    
+    print(gradA1.numpy())
+    print(gradA2.numpy())
     
     print((grad1-grad2).abs().max())
+    
+    
+    torch.manual_seed(seed)
+
+    # P = torch.randn(5, 5)
+    P = torch.randn(5, 5).requires_grad_(True) 
+    
+    jab = torch.autograd.functional.jacobian(lambda P: P.T @ P, P)
+    jab = jab.reshape(25,25)
+    
+    print(jab)
+    print(jab.T @ gradA1.reshape(25))
+    
+    
     assert torch.allclose(grad1, grad2, rtol=1e-2, atol=1e-2)
