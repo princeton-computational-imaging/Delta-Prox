3c3e27f5c236136f868d759e6baf4628526bc81e
zeqiang-lai <laizeqiang@outlook.com>
2023-05-10 21:34:40 +0800

    fix unit test diff linear solver

--- tests/test_linear_solver_torch.py
+++ tests/test_linear_solver_torch.py
@@ -20,7 +20,9 @@ class LinOp(torch.nn.Module):
 
     @property
     def T(self):
-        return LinOp(self.A.T)
+        op = self.clone()
+        op.forward, op.adjoint = op.adjoint, op.forward
+        return op
     
     def clone(self):
         return copy.deepcopy(self)
@@ -39,7 +41,9 @@ class SymmetricLinOp(torch.nn.Module):
 
     @property
     def T(self):
-        return SymmetricLinOp(self.P)
+        op = self.clone()
+        op.forward, op.adjoint = op.adjoint, op.forward
+        return op
     
     def clone(self):
         return copy.deepcopy(self)
@@ -143,3 +147,62 @@ def test_linear_solver_torch_backward_dtheta():
     
     print((grad1-grad2).abs().max())
     assert torch.allclose(grad1, grad2, rtol=1e-2, atol=1e-2)
+
+
+def test_linear_solver_torch_backward_dtheta2():
+    # gradient with implicit differentiation
+    torch.manual_seed(seed)
+
+    P = torch.randn(5, 5)
+    A = LinOp(P @ P.T)
+    
+    x = torch.randn(5)
+    with torch.no_grad():
+        b = A(x)
+    b = b.clone().detach().requires_grad_(True)
+
+    xhat = dprox.proxfn.linalg.linear_solve(A, b)
+    xhat.mean().backward()
+    grad1 = A.A.grad
+    
+    # gradient with unrolling 
+    # warning: 
+    torch.manual_seed(seed)
+
+    P = torch.randn(5, 5)
+    A = LinOp(P @ P.T)
+    
+    x = torch.randn(5)
+    with torch.no_grad():
+        b = A(x)
+    b = b.clone().detach().requires_grad_(True)
+
+    xhat = dprox.proxfn.linalg.solve.conjugate_gradient(A, b)
+    xhat.mean().backward()
+    grad2 = A.A.grad # grad2 is only correct at the diagonal items.
+    
+    
+    # explicit solve
+    torch.manual_seed(seed)
+
+    P = torch.randn(5, 5).requires_grad_(True)
+    A = P@P.T
+    A.retain_grad()
+    x = torch.randn(5)
+    with torch.no_grad():
+        b = A@x
+    b = b.clone().detach().requires_grad_(True)
+    
+    xhat = torch.linalg.solve(A, b)
+    xhat.mean().backward()
+    
+    grad3 = A.grad
+    
+    # summary
+    print('dtheta')
+    print(grad1)
+    print(grad2)
+    print(grad3)
+    
+    print((grad1-grad3).abs().max())
+    assert torch.allclose(grad1, grad3, rtol=1e-2, atol=1e-2)
