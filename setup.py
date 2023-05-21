bb2a8ed8d0ec74474d19e6f9ebf73287538e2a96
zeqiang-lai <laizeqiang@outlook.com>
2023-05-21 23:40:06 +0800

    fix

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 from setuptools import setup, find_packages
 
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
-
-
 deps = [
     'imageio',
     'scikit_image',
@@ -24,7 +19,6 @@ deps = [
 setup(
     name='dprox',
     description='A domain-specific language (DSL) and compiler that transforms optimization problems into differentiable proximal solvers.',
-    long_description=readme(),
     url='https://github.com/princeton-computational-imaging/Delta-Prox',
     author='Zeqiang Lai',
     author_email='laizeqiang@outlook.com',
