0ef8f7c6a66776e5b366f511c3582d216d1d5dd1
Zeqiang-Lai <916323301@qq.com>
2023-05-29 16:06:23 +0800

    support batched cg

--- dprox/linalg/solve/cg.py
+++ dprox/linalg/solve/cg.py
@@ -1,15 +1,63 @@
+from typing import Callable
+
 import torch
 import numpy as np
 
-# TODO: standardize the stopping criterion
-RTOL = 1e-6
-MAX_ITERS = 100
+
+def bdot(x: torch.Tensor, y: torch.Tensor):
+    """
+    Perform batch dot product between two input tensors with the same shape.
+
+    Args:
+      x (torch.Tensor): The input tensor of the shape [batch_size, ...].
+      y (torch.Tensor): Another input tensor with the same shape as x.
+
+    Returns:
+      Batch dot product of the shape [batch_size]
+    """
+    return torch.sum(x.reshape(x.shape[0], -1) * y.reshape(y.shape[0], -1), dim=-1)
+
+
+def expand(x: torch.Tensor, ref: torch.Tensor):
+    """
+    Expands the input tensor to match the number of dimensions of the reference tensor.
+
+    Args:
+      x (torch.Tensor): The input tensor of any shape.
+      ref (torch.Tensor): The reference tensor of any shape.
+
+    Returns:
+      the expanded tensor `x` to match the number of dimensions of the reference tensor `ref`.
+    """
+    while len(x.shape) < len(ref.shape):
+        x = x.unsqueeze(-1)
+    return x
 
 
 def conjugate_gradient(
-    A, b, 
-    x0=None, rtol=RTOL, max_iters=MAX_ITERS, verbose=False
+    A: Callable,
+    b: torch.Tensor,
+    x0: torch.Tensor = None,
+    rtol: float = 1e-6,
+    max_iters: int = 100,
+    verbose: bool = False
 ):
+    """
+    Conjugate gradient method for solving a linear system of equations.
+
+    Args:
+      A (Callable): A is a callable function representing the forward operator A(x) of a matrix free linear operator.
+      b (torch.Tensor): The parameter `b` is a tensor representing the right-hand side of the linear
+    system of equations `Ax = b`.
+      x0 (torch.Tensor): The initial guess for the solution vector. If not provided, it is initialized
+    to a vector of zeros.
+      rtol (float): Relative tolerance for convergence criteria. Default to 1e-6
+      max_iters (int): The maximum number of iterations. Defaults to 100
+      verbose (bool): Whether to logging intermediate information. Defaults to False
+
+    Returns:
+      The solution `x` to the linear system `Ax=b` using the conjugate gradient method.
+    """
 
     # Temp vars
     x = torch.zeros_like(b)
@@ -25,7 +73,7 @@ def conjugate_gradient(
     r *= - 1.0
     r += b
 
-    cg_tol = rtol * torch.linalg.norm(b.ravel(), 2)  # Relative tolerence
+    cg_tol = rtol * torch.linalg.norm(b, 2, dim=(-1, -2))  # Relative tolerence
 
     # CG iteration
     gamma_1 = p = None
@@ -33,13 +81,14 @@ def conjugate_gradient(
     for iter in range(cg_iter):
 
         # Check for convergence
-        normr = torch.linalg.norm(r.ravel(), 2)
-        if normr <= cg_tol:
+        normr = torch.linalg.norm(r, 2, dim=(-1, -2))
+        if torch.all(normr <= cg_tol):
             if verbose:
                 print("Converged at CG Iter %03d" % iter)
             break
 
-        gamma = torch.dot(r.ravel(), r.ravel())
+        gamma = bdot(r, r)
+        gamma = expand(gamma, x)
 
         # direction vector
         if iter > 0:
@@ -54,7 +103,8 @@ def conjugate_gradient(
         # Cg update
         q = Ap
 
-        alpha = gamma / torch.dot(p.ravel(), q.ravel())
+        tmp = bdot(p, q)
+        alpha = gamma / expand(tmp, x)
 
         x = x + alpha * p  # update approximation vector
         r = r - alpha * q  # compute residual
@@ -62,14 +112,14 @@ def conjugate_gradient(
         gamma_1 = gamma
 
     if verbose:
-        print(f'Not converged, r norm={normr.item()}')
-    
+        print(f'Not converged, r norm={normr.tolist()}')
+
     return x
 
 
 def conjugate_gradient2(
-    A, b, 
-    x0=None, rtol=RTOL, max_iters=500, verbose=False
+    A, b,
+    x0=None, rtol=1e-6, max_iters=500, verbose=False
 ):
     # Solves A x = b
     x = torch.ones_like(b)
@@ -101,24 +151,24 @@ def conjugate_gradient2(
 
 
 def PCG(
-    A, b, 
-    Minv=None, x0=None, rtol=RTOL, max_iters=MAX_ITERS, verbose=False
+    A, b,
+    Minv=None, x0=None, rtol=1e-6, max_iters=500, verbose=False
 ):
     """Preconditioned Conjugate Gradient Method"""
     if Minv is None:
-        Minv = lambda x: x
+        def Minv(x): return x
 
     if x0 is not None:
         x = x0
     else:
         x = torch.ones_like(b)
-        
+
     r = A(x) - b
     y = Minv(r)
     p = - y
 
     bnorm = torch.linalg.vector_norm(b.ravel())
-    
+
     for iter in range(max_iters):
         Ap = A(p)
         ry = r.ravel() @ y.ravel()
@@ -130,120 +180,10 @@ def PCG(
         beta = (r.ravel() @ y.ravel()) / ry
         p = - y + beta * p
         rnorm = torch.linalg.vector_norm(r.ravel())
-        
+
         if rnorm < rtol * bnorm:
             break
-    
+
     if verbose:
         print('#IT:', iter + 1)
     return x
-
-
-def cg_batch(A_bmm, B, M_bmm=None, X0=None, rtol=RTOL, atol=0., maxiter=None, verbose=False):
-    """Solves a batch of PD matrix linear systems using the preconditioned CG algorithm.
-    This function solves a batch of matrix linear systems of the form
-        A_i X_i = B_i,  i=1,...,K,
-    where A_i is a n x n positive definite matrix and B_i is a n x m matrix,
-    and X_i is the n x m matrix representing the solution for the ith system.
-    Args:
-        A_bmm: A callable that performs a batch matrix multiply of A and a K x n x m matrix.
-        B: A K x n x m matrix representing the right hand sides.
-        M_bmm: (optional) A callable that performs a batch matrix multiply of the preconditioning
-            matrices M and a K x n x m matrix. (default=identity matrix)
-        X0: (optional) Initial guess for X, defaults to M_bmm(B). (default=None)
-        rtol: (optional) Relative tolerance for norm of residual. (default=1e-3)
-        atol: (optional) Absolute tolerance for norm of residual. (default=0)
-        maxiter: (optional) Maximum number of iterations to perform. (default=5*n)
-        verbose: (optional) Whether or not to print status messages. (default=False)
-    """
-    K, n, m = B.shape
-
-    if M_bmm is None:
-        M_bmm = lambda x: x
-    if X0 is None:
-        X0 = M_bmm(B)
-    if maxiter is None:
-        maxiter = 5 * n
-
-    assert B.shape == (K, n, m)
-    assert X0.shape == (K, n, m)
-    assert rtol > 0 or atol > 0
-    assert isinstance(maxiter, int)
-
-    X_k = X0
-    R_k = B - A_bmm(X_k)
-    Z_k = M_bmm(R_k)
-
-    P_k = torch.zeros_like(Z_k)
-
-    P_k1 = P_k
-    R_k1 = R_k
-    R_k2 = R_k
-    X_k1 = X0
-    Z_k1 = Z_k
-    Z_k2 = Z_k
-
-    B_norm = torch.norm(B, dim=1)
-    stopping_matrix = torch.max(rtol*B_norm, atol*torch.ones_like(B_norm))
-
-    if verbose:
-        print("%03s | %010s %06s" % ("it", "dist", "it/s"))
-
-    optimal = False
-    start = time.perf_counter()
-    for k in range(1, maxiter + 1):
-        start_iter = time.perf_counter()
-        Z_k = M_bmm(R_k)
-
-        if k == 1:
-            P_k = Z_k
-            R_k1 = R_k
-            X_k1 = X_k
-            Z_k1 = Z_k
-        else:
-            R_k2 = R_k1
-            Z_k2 = Z_k1
-            P_k1 = P_k
-            R_k1 = R_k
-            Z_k1 = Z_k
-            X_k1 = X_k
-            denominator = (R_k2 * Z_k2).sum(1)
-            denominator[denominator == 0] = 1e-8
-            beta = (R_k1 * Z_k1).sum(1) / denominator
-            P_k = Z_k1 + beta.unsqueeze(1) * P_k1
-
-        denominator = (P_k * A_bmm(P_k)).sum(1)
-        denominator[denominator == 0] = 1e-8
-        alpha = (R_k1 * Z_k1).sum(1) / denominator
-        X_k = X_k1 + alpha.unsqueeze(1) * P_k
-        R_k = R_k1 - alpha.unsqueeze(1) * A_bmm(P_k)
-        end_iter = time.perf_counter()
-
-        residual_norm = torch.norm(A_bmm(X_k) - B, dim=1)
-
-        # if verbose:
-        #     print("%03d | %8.4e %4.2f" %
-        #           (k, torch.max(residual_norm-stopping_matrix),
-        #             1. / (end_iter - start_iter)))
-
-        if (residual_norm <= stopping_matrix).all():
-            optimal = True
-            break
-
-    end = time.perf_counter()
-
-    if verbose:
-        if optimal:
-            print("Terminated in %d steps (reached maxiter). Took %.3f ms." %
-                  (k, (end - start) * MAX_ITERS0))
-        else:
-            print("Terminated in %d steps (optimal). Took %.3f ms." %
-                  (k, (end - start) * MAX_ITERS0))
-
-
-    info = {
-        "niter": k,
-        "optimal": optimal
-    }
-
-    return X_k, info
--- tests/test_jd23.py
+++ tests/test_jd23.py
@@ -1,3 +1,5 @@
+import torch
+
 from dprox import *
 from dprox.utils import *
 from dprox.utils.examples import *
@@ -14,13 +16,53 @@ def test_jd2():
     x = Variable()
     data_term = sum_squares(mosaic(conv(x, psf)) - b)
     reg_term = deep_prior(x, denoiser='ffdnet_color')
-    prob = Problem(data_term + reg_term, linear_solve_config=LinearSolveConfig(max_iters=500))
+    prob = Problem(data_term + reg_term, linear_solve_config=LinearSolveConfig(max_iters=50))
 
-    max_iter = 24
+    max_iter = 5
     rhos, sigmas = log_descent(35, 30, max_iter)
-    out = prob.solve(method='admm', x0=b, rhos=rhos, lams={reg_term: sigmas}, max_iter=24, pbar=True)
+    out = prob.solve(method='admm', x0=b, rhos=rhos, lams={reg_term: sigmas}, max_iter=max_iter, pbar=True)
 
-    imshow(out)
-    print(psnr(out, img))  # 29.689
+    print(psnr(out, img))  # 25.25
 
-    assert psnr(out, img) - 29.9 < 0.1
+    assert abs(psnr(out, img) - 25.25) < 0.1
+
+
+def load(name='face', return_tensor=True):
+    import imageio
+    s = imageio.imread(name).copy().astype('float32') / 255
+    s = s[:768,:,:]
+    if return_tensor:
+        s = to_torch_tensor(s, batch=True).float()
+    return s
+
+
+def test_jd2_batched():
+    psf = point_spread_function(15, 5)
+    
+    img = load('tests/face2.png')
+    # img = sample('face')
+    img1 = to_torch_tensor(img, batch=True).float()
+    b = blurring(img1, psf)
+    b1 = mosaicing(b)
+    
+    img = sample('face')
+    img2 = to_torch_tensor(img, batch=True).float()
+    b = blurring(img2, psf)
+    b2 = mosaicing(b)
+    b = torch.cat([b1, b2], dim=0)
+    print(b.shape)
+
+    x = Variable()
+    data_term = sum_squares(mosaic(conv(x, psf)) - b)
+    reg_term = deep_prior(x, denoiser='ffdnet_color')
+    prob = Problem(data_term + reg_term, linear_solve_config=LinearSolveConfig(max_iters=50))
+
+    max_iter = 5
+    rhos, sigmas = log_descent(35, 30, max_iter)
+    out = prob.solve(method='admm', x0=b, rhos=rhos, lams={reg_term: sigmas}, max_iter=max_iter, pbar=True)
+
+    print(psnr(out[0:1], img1))  # 29.689
+    print(psnr(out[1].unsqueeze(0), img2))  # 29.689
+
+    assert abs(psnr(out[0:1], img1) - 29.92) < 0.1
+    assert abs(psnr(out[1].unsqueeze(0), img2) - 25.25) < 0.1
