9e234366ba66b4b0092fa67bc9f0238fdaf84de8
Zeqiang-Lai <916323301@qq.com>
2023-06-30 09:58:30 +0800

    update optics example

--- examples/computional_optics/test_deconv.py
+++ examples/computional_optics/test_deconv.py
@@ -1,24 +1,26 @@
-from dprox.utils import *
-from dprox.linop.conv import conv_doe
-from dprox import Variable
-from dprox import *
-from doe_model import img_psf_conv
-from common import (build_baseline_profile, build_doe_model, circular, device,
-                    load_sample_img, plot, sanity_check, subplot)
-import torchvision.utils
-import torchlight.nn as tlnn
-import torchlight as tl
-import torch.nn as nn
-import torch
-import sys
 import os
 from pathlib import Path
-sys.path.append('../')
+
+import torch
+import torch.nn as nn
+import torchlight as tl
+import torchlight.nn as tlnn
+
+from dprox import *
+from dprox import Variable
+from dprox.linop.conv import conv_doe
+from dprox.utils import *
+from dprox.utils.examples.optic.common import (build_doe_model, DOEModelConfig, load_sample_img)
+from dprox.utils.examples.optic.doe_model import img_psf_conv
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 @torch.no_grad()
 def main():
-    rgb_collim_model = build_doe_model()
+    config = DOEModelConfig()
+    rgb_collim_model = build_doe_model(config).to(device)
+    circular = config.circular
 
     # -------------------- Define Model --------------------- #
     x = Variable()
@@ -26,14 +28,7 @@ def main():
     PSF = Placeholder()
     data_term = sum_squares(conv_doe(x, PSF, circular=circular), y)
     reg_term = deep_prior(x, denoiser='ffdnet_color')
-    solver = compile(data_term + reg_term, method='admm',
-                     linear_solve_config=dict(
-                         num_iters=50,
-                         verbose=False,
-                         lin_solver_type='custom2',
-                         tol=1e-5,
-                         height_map=rgb_collim_model.height_map,
-                     ))
+    solver = compile(data_term + reg_term, method='admm')
     solver.eval()
 
     # ---------------- Setup Hyperparameter ----------------- #
@@ -54,23 +49,11 @@ def main():
         y.value = inp
         PSF.value = psf
 
-        timer = tl.utils.Timer()
-        timer.tic()
-
         out = solver.solve(x0=inp,
                            rhos=rgb_collim_model.rhos,
-                           lams={reg_term: rgb_collim_model.sigmas},
+                           lams={reg_term: rgb_collim_model.sigmas.sqrt()},
                            max_iter=max_iter)
-
-        print(timer.toc())
         return gt, inp, out
-
-    # ----------------- Sanity Check ------------------------ #
-    gt, ob = sanity_check(rgb_collim_model.get_psf())
-    gt, inp, out = step_fn(gt)
-    print(gt.shape, ob.shape, out.shape)
-    print(tl.metrics.psnr(out, gt))  # 38.55
-    imshow(gt, ob, out)
 
     print('Model size (M)', tlnn.benchmark.model_size(rgb_collim_model))
     print('Trainable model size (M)', tlnn.benchmark.trainable_model_size(rgb_collim_model))
@@ -116,25 +99,6 @@ def main():
         logger.info(tracker.summary())
 
         print(timer.toc() / len(os.listdir(root)))
-
-    # # --------------- Save Results ------------------ #
-    # torchvision.utils.save_image(inp, imgdir / f'inp.png')
-    # torchvision.utils.save_image(pred, imgdir / f'pred.png')
-    # torchvision.utils.save_image(gt, imgdir / f'gt.png')
-
-    # height_map = rgb_collim_model.height_map.height_map_sqrt.detach()**2
-    # plot(height_map, imgdir / f'height_map.png')
-    # psf = rgb_collim_model.get_psf()
-    # subplot(psf, imgdir / f'psf.png')
-    # import matplotlib.pyplot as plt
-    # img = psf[0].permute(1,2,0).detach().cpu().numpy()
-    # # img = np.log(img)
-    # plt.figure()
-    # plt.imshow(img)
-    # plt.colorbar()
-    # plt.savefig(imgdir/'psf_rgb.png')
-    # phase = torch.angle(rgb_collim_model.height_map.get_phase_profile())
-    # subplot(phase, imgdir / f'phase.png')
 
 
 main()
