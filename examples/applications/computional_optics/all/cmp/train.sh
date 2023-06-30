fcccdf1c68f55a402b951d6bede59f18deede92f
Zeqiang-Lai <916323301@qq.com>
2023-06-30 21:13:40 +0800

    update optics

--- examples/applications/computional_optics/all/cmp/train.sh
+++
@@ -1,2 +0,0 @@
-python train_e2e.py --sigma 7.65 --bs 8 --savedir saved/train_deconv_doe --root data/div2k
-python train_e2e_rcan.py --sigma 7.65 --bs 1 --savedir saved/train_deconv_doe_rcan --root data/div2k
--- examples/applications/computional_optics/pnp_optics.py
+++ examples/applications/computional_optics/pnp_optics.py
@@ -1,16 +1,27 @@
-import sys
-sys.path.append('../')
-
+import torchlight as tl
+import os
 import torch
 
 from dprox import *
 from dprox.utils import *
 from dprox import Variable
 from dprox.linop.conv import conv_doe
+from dprox.utils.examples.optic.common import (
+    build_doe_model, load_sample_img, build_baseline_profile,
+    DOEModelConfig
+)
+from dprox.utils.examples.optic.doe_model import img_psf_conv
 
-from doe_model import img_psf_conv
-from common import (build_doe_model,build_baseline_profile, 
-                    circular, load_sample_img, sanity_check, device)
+
+# -------------------- Define Model --------------------- #
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+config = DOEModelConfig()
+rgb_collim_model = build_doe_model(config).to(device)
+circular = config.circular
+fresnel_phase_c = build_baseline_profile(rgb_collim_model)
+
+# -------------------- Define Solver --------------------- #
 
 x = Variable()
 y = Placeholder()
@@ -18,8 +29,9 @@ PSF = Placeholder()
 data_term = sum_squares(conv_doe(x, PSF, circular=circular), y)
 reg_term = deep_prior(x, denoiser='ffdnet_color')
 solver = compile(data_term + reg_term, method='admm')
-rgb_collim_model = build_doe_model()
-fresnel_phase_c = build_baseline_profile(rgb_collim_model)
+solver.eval()
+
+# -------------------- Define Forward --------------------- #
 
 sigma = 7.65 / 255
 max_iter = 10
@@ -27,24 +39,19 @@ rhos, sigmas = log_descent(49, 7.65, max_iter, sigma=max(0.255 / 255, sigma))
 
 def step_fn(gt):
     gt = gt.to(device).float()
-    # psf = rgb_collim_model.get_psf()
     psf = rgb_collim_model.get_psf(fresnel_phase_c)
     inp = img_psf_conv(gt, psf, circular=circular)
     inp = inp + torch.randn(*inp.shape, device=inp.device) * sigma
     y.value = inp
     PSF.value = psf
-    timer = tl.utils.Timer()
-    timer.tic()
     out = solver.solve(x0=inp,
-                        rhos=rhos,
-                        lams={reg_term: sigmas},
-                        max_iter=max_iter)
-    print(timer.toc())
+                       rhos=rhos,
+                       lams={reg_term: sigmas},
+                       max_iter=max_iter)
     return gt, inp, out
-    
-    
-import os
-import torchlight as tl
+
+# -------------------- Evalution --------------------- #
+
 tl.metrics.set_data_format('chw')
 
 # datasets = ['McMaster', 'Kodak24']
@@ -52,7 +59,7 @@ datasets = ['Urban100']
 
 for dataset in datasets:
     root = 'data/test/' + dataset
-    logger = tl.logging.Logger('results/deconv_doe/' + dataset, name=dataset)
+    logger = tl.logging.Logger('saved/pnp/results' + dataset, name=dataset)
     tracker = tl.trainer.util.MetricTracker()
 
     timer = tl.utils.Timer()
@@ -67,13 +74,13 @@ for dataset in datasets:
 
         psnr = tl.metrics.psnr(pred, gt)
         ssim = tl.metrics.ssim(pred, gt)
-        
+
         tracker.update('psnr', psnr)
         tracker.update('ssim', ssim)
-        
-        logger.info('{} PSNR {} SSIM {}'.format(name, psnr, ssim))  
+
+        logger.info('{} PSNR {} SSIM {}'.format(name, psnr, ssim))
         logger.save_img(name, pred)
 
     logger.info('averge results')
     logger.info(tracker.summary())
-    print(timer.toc()/len(os.listdir(root)))
+    print(timer.toc() / len(os.listdir(root)))
