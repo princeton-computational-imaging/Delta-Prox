22ddcd34a33579f9ac772b33b02d8a8c4eeab003
Zeqiang Lai <laizeqiang@outlook.com>
2023-11-14 19:42:42 +0800

    add e2e optics joint

--- dprox/contrib/restoration.py
+++ dprox/contrib/restoration.py
@@ -1,31 +1,32 @@
+import numpy as np
 import scipy.misc
 import scipy.ndimage
-import numpy as np
+import torch
 
-from dprox.utils import to_torch_tensor, to_ndarray
+from dprox.utils import to_ndarray, to_torch_tensor
 
 samples = {
-    'face': scipy.misc.face(),
-    'ascent': scipy.misc.ascent(),
+    "face": scipy.misc.face(),
+    "ascent": scipy.misc.ascent(),
 }
 
 
-def sample(name='face', return_tensor=True):
-    s = samples[name].copy().astype('float32') / 255
+def sample(name="face", return_tensor=True):
+    s = samples[name].copy().astype("float32") / 255
     if return_tensor:
         s = to_torch_tensor(s, batch=True).float()
     return s
 
 
 def point_spread_function(ksize, sigma):
-    return np.expand_dims(fspecial_gaussian(ksize, sigma), axis=2).astype('float32')
+    return np.expand_dims(fspecial_gaussian(ksize, sigma), axis=2).astype("float32")
 
 
 def blurring(img, psf):
     device = img.device
     img = to_ndarray(img, debatch=True)
     psf = to_ndarray(psf)
-    b = scipy.ndimage.filters.convolve(img, psf, mode='wrap')
+    b = scipy.ndimage.filters.convolve(img, psf, mode="wrap")
     b = to_torch_tensor(b, batch=True).to(device)
     return b
 
@@ -46,25 +47,25 @@ def fspecial_gaussian(hsize, sigma):
 
 def downsampling(img, psf, sf):
     import cv2
+
     device = img.device
     img = to_ndarray(img, debatch=True)
     psf = to_ndarray(psf)
-    blurred = scipy.ndimage.filters.convolve(img, psf, mode='wrap')
+    blurred = scipy.ndimage.filters.convolve(img, psf, mode="wrap")
     downed = blurred[0::sf, 0::sf, ...]
     x0 = cv2.resize(downed, (downed.shape[1] * sf, downed.shape[0] * sf), interpolation=cv2.INTER_CUBIC)
-    
+
     x0 = to_torch_tensor(x0, batch=True).to(device).float()
     downed = to_torch_tensor(downed, batch=True).to(device).float()
     return downed, x0
 
 
-
 def masks_CFA_Bayer(shape):
-    pattern = 'RGGB'
-    channels = dict((channel, np.zeros(shape)) for channel in 'RGB')
+    pattern = "RGGB"
+    channels = dict((channel, np.zeros(shape)) for channel in "RGB")
     for channel, (y, x) in zip(pattern, [(0, 0), (0, 1), (1, 0), (1, 1)]):
         channels[channel][y::2, x::2] = 1
-    return tuple(channels[c].astype(bool) for c in 'RGB')
+    return tuple(channels[c].astype(bool) for c in "RGB")
 
 
 def mosaicing(img):
@@ -82,5 +83,15 @@ def mosaicing_np(img):
     shape = img.shape[:2]
     R_m, G_m, B_m = masks_CFA_Bayer(shape)
     mask = np.concatenate((R_m[..., None], G_m[..., None], B_m[..., None]), axis=-1)
+    b = mask * img
+    return b
+
+
+def mosaicing_torch(img):
+    shape = img.shape[-2:]
+    R_m, G_m, B_m = masks_CFA_Bayer(shape)
+    mask = np.concatenate((R_m[..., None], G_m[..., None], B_m[..., None]), axis=-1)
+    mask = torch.from_numpy(mask)
+    mask = mask.permute(2, 0, 1).unsqueeze(0).to(img.device)
     b = mask * img
     return b
---
+++ examples/papers/deltaprox_siggraph_2023/computional_optics/e2e_optics_dprox_joint.py
@@ -0,0 +1,237 @@
+import os
+from pathlib import Path
+
+import fire
+import imageio
+import torch
+import torch.nn as nn
+import torchlight as tl
+import torchlight.nn as tlnn
+
+from dprox import *
+from dprox import Variable
+from dprox.contrib import mosaicing_torch
+from dprox.contrib.optic import (
+    DOEModelConfig,
+    build_doe_model,
+    img_psf_conv,
+    load_sample_img,
+    normalize_psf,
+)
+from dprox.linop.conv import conv_doe
+from dprox.utils import *
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def build_model():
+    config = DOEModelConfig()
+    rgb_collim_model = build_doe_model(config).to(device)
+    circular = config.circular
+
+    # -------------------- Define Model --------------------- #
+    x = Variable()
+    y = Placeholder()
+    PSF = Placeholder()
+    data_term = sum_squares(mosaic(conv_doe(x, PSF, circular=circular)), y)
+    reg_term = deep_prior(x, denoiser="ffdnet_color")
+    solver = compile(data_term + reg_term, method="admm")
+    solver.eval()
+
+    # ---------------- Setup Hyperparameter ----------------- #
+    max_iter = 10
+    sigma = 7.65 / 255.0
+    rhos, sigmas = log_descent(49, 7.65, max_iter, sigma=max(0.255 / 255, sigma))
+    rgb_collim_model.rhos = nn.parameter.Parameter(rhos)
+    rgb_collim_model.sigmas = nn.parameter.Parameter(sigmas)
+
+    # ---------------- Forward Model ------------------------ #
+    def step_fn(gt):
+        gt = gt.to(device).float()
+        psf = rgb_collim_model.get_psf()
+        inp = img_psf_conv(gt, psf, circular=circular)
+        inp = mosaicing_torch(inp)
+        inp = inp + torch.randn(*inp.shape, device=inp.device) * sigma
+        y.value = inp
+        PSF.value = psf
+
+        out = solver.solve(x0=inp, rhos=rgb_collim_model.rhos, lams={reg_term: rgb_collim_model.sigmas.sqrt()}, max_iter=max_iter)
+        return gt, inp, out
+
+    print("Model size (M)", tlnn.benchmark.model_size(rgb_collim_model))
+    print("Trainable model size (M)", tlnn.benchmark.trainable_model_size(rgb_collim_model))
+
+    return solver, rgb_collim_model, step_fn
+
+
+@torch.no_grad()
+def eval(step_fn, result_dir, dataset):
+    tl.metrics.set_data_format("chw")
+
+    root = hf.download_dataset(dataset)
+    dataset = os.path.basename(root)
+    logger = tl.logging.Logger(os.path.join(result_dir, dataset), name=dataset)
+    tracker = tl.trainer.util.MetricTracker()
+
+    timer = tl.utils.Timer()
+    timer.tic()
+    for idx, name in enumerate(list_image_files(root)):
+        gt = load_sample_img(os.path.join(root, name))
+
+        torch.manual_seed(idx)
+        torch.cuda.manual_seed(idx)
+        gt, inp, pred = step_fn(gt)
+        pred = pred.clamp(0, 1)
+
+        psnr = tl.metrics.psnr(pred, gt)
+        ssim = tl.metrics.ssim(pred, gt)
+
+        tracker.update("psnr", psnr)
+        tracker.update("ssim", ssim)
+
+        logger.save_img(name, pred)
+        logger.save_img(name + "_inp.png", inp)
+        logger.save_img(name + "_gt.png", gt)
+
+    logger.info("averge results")
+    logger.info(tracker.summary())
+    print("Average Running Time", timer.toc() / len(os.listdir(root)))
+
+    return tracker["psnr"]
+
+
+def test(
+    dataset="Urban100",
+    checkpoint="computational_optics/joint_dprox.pth",
+    result_dir="results/e2e_optics_dprox_joint",
+):
+    _, rgb_collim_model, step_fn = build_model()
+    ckpt = hf.load_checkpoint(checkpoint)
+    rgb_collim_model.load_state_dict(ckpt["model"])
+
+    return eval(step_fn, result_dir=result_dir, dataset=dataset)
+
+
+def train(
+    training_dataset="BSD500",
+    savedir="saved/e2e_optics_dprox_joint/",
+    epochs=10,
+    bs=2,
+    lr=1e-4,
+    resume=None,
+):
+    import torch.nn.functional as F
+    from torch.utils.data.dataloader import DataLoader
+    from tqdm import tqdm
+
+    from dprox.contrib.optic import Dataset
+
+    print("Training on device", device)
+
+    _, rgb_collim_model, step_fn = build_model()
+
+    savedir = Path(savedir)
+    savedir.mkdir(exist_ok=True, parents=True)
+    logger = tl.logging.Logger(savedir)
+
+    # ----------------- Start Training ------------------------ #
+    root = hf.download_dataset(training_dataset, force_download=False)
+    dataset = Dataset(root)
+    loader = DataLoader(dataset, batch_size=bs, shuffle=True)
+
+    optimizer = torch.optim.AdamW(rgb_collim_model.parameters(), lr=1e-4, weight_decay=1e-3)
+    tlnn.utils.adjust_learning_rate(optimizer, lr)
+    # scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, epochs, eta_min=1e-6)
+
+    epoch = 0
+    gstep = 0
+    best_psnr = 0
+    imgdir = savedir / "imgs"
+    imgdir.mkdir(exist_ok=True, parents=True)
+
+    if resume:
+        ckpt = torch.load(savedir / resume)
+        rgb_collim_model.load_state_dict(ckpt["model"])
+        optimizer.load_state_dict(ckpt["optimizer"])
+        epoch = ckpt["epoch"] + 1
+        gstep = ckpt["gstep"] + 1
+        best_psnr = ckpt["best_psnr"]
+
+    def save_ckpt(name, psnr=0):
+        ckpt = {
+            "model": rgb_collim_model.state_dict(),
+            "optimizer": optimizer.state_dict(),
+            "epoch": epoch,
+            "gstep": gstep,
+            "psnr": psnr,
+            "best_psnr": best_psnr,
+        }
+        torch.save(ckpt, savedir / name)
+
+    save_ckpt("last.pth")
+    history_psf = []
+    history_psnr = []
+
+    while epoch < epochs:
+        tracker = tl.trainer.util.MetricTracker()
+        pbar = tqdm(total=len(loader), dynamic_ncols=True, desc=f"Epcoh[{epoch}]")
+
+        for i, batch in enumerate(loader):
+            # if gstep % 10 == 0:
+            #     # validate
+            #     with torch.no_grad():
+            #         psnr = eval(
+            #             step_fn=step_fn,
+            #             dataset='data/visual',
+            #             result_dir='results_eval',
+            #         )
+            #         if psnr > best_psnr:
+            #             best_psnr = psnr
+            #             save_ckpt('best.pth')
+            #         logger.info('Validate Epoch {} PSNR={} Best PSNR={}'.format(epoch, psnr, best_psnr))
+
+            #     psf = rgb_collim_model.get_psf()
+            #     psf = crop_center_region(
+            #         normalize_psf(to_ndarray(psf, debatch=True), clip_percentile=0.01)
+            #     )
+            #     imageio.imsave(imgdir / f'psf_{gstep}.png', psf)
+            #     history_psf.append(psf)
+            #     history_psnr.append(psnr)
+
+            #     with open(savedir / 'stat.txt', 'a') as f:
+            #         f.write(f'{gstep} {psnr}\n')
+
+            gt, inp, pred = step_fn(batch)
+
+            loss = F.mse_loss(gt, pred)
+            loss.backward()
+
+            optimizer.step()
+            optimizer.zero_grad()
+
+            psnr = tl.metrics.psnr(pred, gt)
+            loss = loss.item()
+            tracker.update("loss", loss)
+            tracker.update("psnr", psnr)
+
+            pbar.set_postfix({"loss": f'{tracker["loss"]:.4f}', "psnr": f'{tracker["psnr"]:.4f}'})
+            pbar.update()
+
+            gstep += 1
+
+        logger.info("Epoch {} Loss={} LR={}".format(epoch, tracker["loss"], tlnn.utils.get_learning_rate(optimizer)))
+
+        save_ckpt("last.pth", tracker["psnr"])
+        pbar.close()
+        epoch += 1
+
+    imageio.mimsave(savedir / "psf.mp4", history_psf)
+
+
+if __name__ == "__main__":
+    fire.Fire(
+        {
+            "train": train,
+            "test": test,
+        }
+    )
